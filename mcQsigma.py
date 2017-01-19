f8ec90da57fe1ce98f690c60ca434a3834a92085
Meep <kldeasis@gmail.com>
2017-01-18 22:15:56 -0700

    created multitilecoder wrapper to make Q cleaner

--- mcQsigma.py
+++ mcQsigma.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import numpy as np
 from random import randint, random, choice, seed
 
-from tilecoding import tilecoder
+from multitilecoding import multitilecoder
 import mountaincar
 
 class QSigma:
@@ -14,12 +14,10 @@ class QSigma:
     # num actions
     self._n_actions = 3
     # tilecoder
-    tilings = 25
-    dims = [16, 16]
+    tilings = 5
+    dims = [8, 8]
     lims = [(-1.2, 0.5), (-0.07, 0.07)]
-    self._Q = [0] * self._n_actions
-    for i in range(self._n_actions):
-      self._Q[i] = tilecoder(dims, lims, tilings, step_size)
+    self._Q = multitilecoder(self._n_actions, dims, lims, tilings, step_size)
     # eps greedy
     self._eps = epsilon
 
@@ -33,7 +31,7 @@ class QSigma:
     action = self.pick_action(self._s)
     self._tr = [(self._s, self._r_sum)] * self._n
     self._delta = [0.0] * self._n
-    self._Qt = [self._Q[action][self._s]] * (self._n + 1)
+    self._Qt = [self._Q[self._s, action]] * (self._n + 1)
     self._pi = [0.0] * self._n
     self._sigma = [0.0] * self._n
     while (self._tau != (self._T - 1)) and (self._time < max_steps):
@@ -51,9 +49,9 @@ class QSigma:
             self._delta[self._time%self._n] = r - self._Qt[self._time%(self._n+1)] # TD error
         else: # commit the next action
             action = self.pick_action(sp) # select arbitrarily and store an action as A_(t+1)
-            self._Qt[(self._time + 1)%(self._n+1)] = self._Q[action][sp] # Store Q(St+1;At+1) as Qt+1
+            self._Qt[(self._time + 1)%(self._n+1)] = self._Q[sp, action] # Store Q(St+1;At+1) as Qt+1
             self._sigma[(self._time+1)%self._n] = self._init_sigma
-            self._delta[self._time%self._n] = r - self._Qt[self._time%(self._n+1)] + discount*((1-self._sigma[(self._time+1)%self._n]) * self.expected_Q(sp) + self._sigma[(self._time+1)%self._n] * self._Q[action][sp])
+            self._delta[self._time%self._n] = r - self._Qt[self._time%(self._n+1)] + discount*((1-self._sigma[(self._time+1)%self._n]) * self.expected_Q(sp) + self._sigma[(self._time+1)%self._n] * self._Q[sp, action])
             self._pi[(self._time+1)%self._n] = self.get_action_probability(sp, action)
         self._s = sp # update agent state
     self._tau = self._time + 1 - self._n # time whose estimate is being updated
@@ -64,17 +62,17 @@ class QSigma:
             G += E * self._delta[k%self._n]
             E *= discount * ((1-self._sigma[(k+1)%self._n]) * self._pi[(k+1)%self._n] + self._sigma[(k+1)%self._n])
         s, a = self._tr[self._tau%self._n]
-        self._Q[a][s] = G
+        self._Q[s, a] = G
     self._time += 1
     return action # return the committed next action
 
   def get_action_probability(self, state, action):
     """ return the action probability at a state of a given policy P[s][a] """
-    Q = [0.0] * self._n_actions
+    Qs = [0.0] * self._n_actions
     for a in range(self._n_actions):
-      Q[a] = self._Q[a][state]
-    if Q[action] == max(Q):
-        return self._eps/self._n_actions + (1.0 - self._eps)/Q.count(max(Q))
+      Qs[a] = self._Q[state, a]
+    if Qs[action] == max(Qs):
+        return self._eps/self._n_actions + (1.0 - self._eps)/Qs.count(max(Qs))
     else:
         return self._eps/self._n_actions
 
@@ -83,11 +81,11 @@ class QSigma:
     if random() < self._eps:
       return randint(0, self._n_actions - 1)
     else:
-      Q = [0.0] * self._n_actions
+      Qs = [0.0] * self._n_actions
       for a in range(self._n_actions):
-        Q[a] = self._Q[a][state]
-      max_Q = max(Q)
-      indices = [i for i, x in enumerate(Q) if x == max_Q]
+        Qs[a] = self._Q[state, a]
+      max_Q = max(Qs)
+      indices = [i for i, x in enumerate(Qs) if x == max_Q]
       if len(indices) == 1:
           return indices[0]
       else: # break ties randomly to prevent the case where committing to one action leads to no reward
@@ -95,12 +93,12 @@ class QSigma:
 
   def expected_Q(self, state):
     """ get the expected Q under a target policy """
-    Q = [0.0] * self._n_actions
+    Qs = [0.0] * self._n_actions
     for a in range(self._n_actions):
-      Q[a] = self._Q[a][state]
-    Q_exp = (1.0 - self._eps) * max(Q)
+      Qs[a] = self._Q[state, a]
+    Q_exp = (1.0 - self._eps) * max(Qs)
     for a in range(self._n_actions):
-      Q_exp += (self._eps / self._n_actions) * Q[a]
+      Q_exp += (self._eps / self._n_actions) * Qs[a]
     return Q_exp
 
 def example():
@@ -120,10 +118,11 @@ def example():
   z = np.zeros([len(y), len(x)])
   for i in range(len(x)):
     for j in range(len(y)):
-      Q_max = agent._Q[0][x[i], y[j]]
+      s = (x[i], y[j])
+      Q_max = agent._Q[s, 0]
       for a in range(1, agent._n_actions):
-        if agent._Q[a][x[i], y[j]] > Q_max:
-          Q_max = agent._Q[a][x[i], y[j]]
+        if agent._Q[s, a] > Q_max:
+          Q_max = agent._Q[s, a]
       z[j, i] = -Q_max
 
   # plot
---
+++ multitilecoding.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from tilecoding import tilecoder
+
+'''
+creates a list of identical tile coders - convenient for Q[s, a] with discrete, independent actions
+usage:
+Q = multitilecoder(n_actions, dims, limits, tilings, step_size)
+Q[s, a] = r + gamma * Q[sp, ap]
+print(Q[s, a])
+'''
+
+class multitilecoder:
+  def __init__(self, n_tilecoders, dims, limits, tilings, step_size=0.1):
+    self._n = n_tilecoders
+    self._tcs = [tilecoder(dims, limits, tilings, step_size) for i in range(self._n)]
+
+  def set_step_size(index, step_size):
+    self._tcs[index].set_step_size(step_size)
+
+  def set_step_sizes(step_size):
+    for tc in self._tcs:
+      tc.set_step_size(step_size)
+
+  # get value of tilecoder x[1] at location x[0]
+  def __getitem__(self, x):
+    return self._tcs[x[1]][x[0]]
+
+  # set value of tilecoder x[1] at location x[0] to val
+  def __setitem__(self, x, val):
+    self._tcs[x[1]][x[0]] = val
