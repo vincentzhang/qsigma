f278f1f0521113315bcba22eb479cfebcf73f498
Meep <kldeasis@gmail.com>
2017-02-21 07:16:30 -0700

    plot

--- results/plot_rw.py
+++ results/plot_rw.py
@@ -10,7 +10,7 @@ alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 sigmas = [0.0, 0.25, 0.5, 0.75, 1.0,]
 
 #[steps, alpha, sigma, run, ep]
-eps = np.arange(1, 101)
+eps = np.arange(1, 51)
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
@@ -20,7 +20,7 @@ ax.set_ylabel('RMS Error')
 ni = 2
 ai = 3
 for i in range(len(sigmas) - 1):
-  ax.plot(eps, rmse.mean(3)[ni][ai][i], label='σ = ' + str(sigmas[i]))
-ax.plot(eps, rmsed.mean(3)[ni][ai][0], label='β = 0.9')
+  ax.plot(eps, rmse.mean(3)[ni][ai][i][1:51], label='σ = ' + str(sigmas[i]))
+ax.plot(eps, rmsed.mean(3)[ni][ai][0][1:51], label='β = 0.9')
 ax.legend()
 plt.show()
